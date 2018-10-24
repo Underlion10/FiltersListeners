25cf1586ea2ca9e0b5ee4fa5a26d296fb9922da1
lione <lione@DESKTOP-AV83DIO>
2018-10-24 20:17:46 +0100

    2do commit - listeners

---
+++ src/com/filterlisteners/listener/Listener.java
@@ -0,0 +1,87 @@
+package com.filterlisteners.listener;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class Listener
+ *
+ */
+@WebListener
+public class Listener implements ServletContextListener, HttpSessionListener {
+
+    /**
+     * Default constructor. 
+     */
+    public Listener() {
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent se)  {
+    	Connection conn = (Connection) se.getSession().getServletContext().getAttribute("dbConn");
+    	String username = (String) se.getSession().getAttribute("user");
+    	Statement stm;
+		try {
+			stm = conn.createStatement();
+			stm.executeUpdate("update users set actualSession = " + 
+			se.getSession().getId() + " where username = " + username);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent se)  {
+    	Connection conn = (Connection) se.getSession().getServletContext().getAttribute("dbConn");
+    	String username = (String) se.getSession().getAttribute("user");
+    	Statement stm;
+		try {
+			stm = conn.createStatement();
+			stm.executeUpdate("update users set actualSession = null" + " where username = " + username);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  {
+    	try {
+			Connection conn = (Connection) sce.getServletContext().getAttribute("dbConn");
+			conn.close();
+			sce.getServletContext().removeAttribute("dbConn");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  {
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost/baseEjemplo?useLegacyDatetimeCode=" + "false&serverTimezone=GMT", "root", "");
+			sce.getServletContext().setAttribute("dbConn", conn);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    }
+	
+}
